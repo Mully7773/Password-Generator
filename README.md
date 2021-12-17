d1d714b3fe05ac7b22ad61c1fd2feec806ab6243
Nick M <mully7773@gmail.com>
2021-12-16 19:54:13 -0600

    README completion, complete password display logic, delete practice scripts, add parseInt

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Password-Generator
-This is a password generator I made using JavaScript.
+
+## Introduction:
+
+This is a password generator I helped make using JavaScript. The HTML and CSS as well as a little bit of
+starter JavaScript were provided to me. This project is
+compatible with smartphones, tablets, and laptops. I spent a considerable amount of time playing with the logic.
+
+## General Information:
+
+In this project, I used JavaScript to make the password generator functional. Users can change the outcome of the 
+randomly generated password based on certain criteria like numbers, uppercase letters, symbols, etc. They may also
+select the length of their password from 8 to 126 characters.
+
+
+ I used the following JavaScript tools:
+1. Variables (implementation of ECMA2015 JS)
+2. Functions (implementation of ECMA2015 JS)
+3. Arrays
+4. For Loops
+5. Methods
+
+## Screenshots:
+
+![Screenshot of full screen](./screenshots/S0.png)
+![Screenshot of small mobile](./screenshots/S1.png)
+![Screenshot of full screen](./screenshots/S2.png)
+
+
+
+## Built with:
+
+1. Visual Studio Code
+2. HTML
+3. CSS
+4. JavaScript
+5. Chrome DevTools
+6. A little bit of markdown
+
+## Author:
+
+Nick Mullenmeister
--- script.js
+++ script.js
@@ -1,56 +1,3 @@
-// // Assignment Code
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-//Need a series of .prompt on click
-
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-
-//Need to use .confirm for these
-
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-
-//Use a conditional--something like...
-// if (password.length > 8 && password.length <= 128)
-// we want the user to choose a number between 8 and 128
-
-
-
-// WHEN prompted for character types to include in the password
-// THEN I choose lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
-
-//add prompt function for userInput for num of char in password
-// num should be >=8 and < =128 if/else
-//userINput confirmation for numeric, lowercase, uppercase, and special characters separately
-//save those confirmations in separate variables
-// store all the values in form of arrays
- //var numeric = [0,1....]
- // based on user confirmation pick the correct arrays and concatenate them and store that concatenated array
- //in a new variable
- // loop num times (see second step)
- //generate random num for concat array and grab the item this is based on the num value the user put it
- //push that item in new password array
- //return new password
-
- //var numOfChar = prompt(How many characters?)
-//  if (!(numOfChar >= 8 && numOfChar < 128)) {
-   //   return;
-//  }
-
-
-
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -61,19 +8,20 @@ function writePassword() {
 }
 var generateBtn = document.querySelector("#generate");
 
-// let finalPass = "";
+// These are my arrays of items stored in varibles.
 
 let lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let upperLetters = ["A", "B", "C" , "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let numbers = [0, 1, 2, 3 , 4, 5, 6, 7, 8, 9];
 let symbolics = [" ", "!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~"];
 
-
+// Here is the function I use to add the items of the arrays into a new array. I sort them based on conditional statements.
+// I'm a bit familiar with let, const, and arrow functions, which I studied before the bootcamp. I thought I would try to use them this time.
 
 let generatePassword = () => {
   let finalArray = [];
-  let numOfChar = prompt("Please enter a password length between 8 and 128 characters.");
-    if (numOfChar >= 8 && numOfChar <= 128) {
+  let numOfChar = parseInt(prompt("Please enter a password length between 8 and 128 characters.")); // Use parseInt to ensure the return of an integer
+    if (numOfChar >= 8 && numOfChar <= 128) { console.log("adfa")
     } else {
       alert("Invalid entry.");
       return;
@@ -82,80 +30,36 @@ let generatePassword = () => {
    if (lowerCase) {
      finalArray.push(...lowerLetters);
     }
-    else {
-
-     }
   let upperCase = confirm("Do you want uppercase characters?");
      if (upperCase) {
        finalArray.push(...upperLetters);
-     } else {
-  
      }
   let numerals = confirm("Do you want to include numerals in your password?");
      if(numerals) {
        finalArray.push(...numbers);
-     } else {
-    
-     }
+     } 
   let symbols = confirm("Do you want to include special characters in your password?");
      if(symbols) {
        finalArray.push(...symbolics);
-     } else {
-    
      }
-  for (let i = 0; i < numOfChar; i++) {
-    // let randomArray = finalArray[Math.floor(Math.random() * finalArray.length)];
-    // console.log(finalArray[randomArray])
-    let randomArray = Math.floor(Math.random() * finalArray.length)
-    let lastArray = finalArray[randomArray]
-    console.log(lastArray);
-  }
-  return finalArray;
-}
 
+// This "if" statement is used if the user decides to cancel all of the conditionals laid out above.
+
+     if (!lowerCase && !upperCase && !numerals && !symbols) {
+       alert("Please specify at least one category.")
+       return;
+     }
+
+// I made one last empty array to store the last password after finalArray is randomized in the for loop.
+
+let finalPassword = [];
+  for (let i = 0; i < numOfChar; i++) {
+    let randomArray = Math.floor(Math.random() * finalArray.length)
+    finalPassword.push(finalArray[randomArray])
+  }
+  
+  return finalPassword.join(" ");  // At last we return the randomized password. I used .join(" ") to form a string and eliminate commas between array elements.
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-//The issue with the code below: I only get one random letter from each array.
-//Maybe it would be better to make a single array with all of the characters, letters, numbers, etc.
-//Is it possible to use a string with all values...?
-//Then I could use the .filter with if/else statements if the user selects "cancel".
-//At the end, I'll have to somehow randomize the final password.
-//Another issue is how to apply the password to the user's desired password length.
-
-
-
-// let lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// let randomLowerLetters = Math.floor(Math.random() * lowerLetters.length);
-// console.log(lowerLetters[randomLowerLetters]); // provides one random lowercase letter
-
-// let upperLetters = ["A", "B", "C" , "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-// let randomUpperLetters = Math.floor(Math.random() * upperLetters.length);
-// console.log(upperLetters[randomUpperLetters]); // provides one random lowercase letter
-
-
-
-
-
-// let numbers = [0, 1, 2, 3 , 4, 5, 6, 7, 8, 9];
-
-// let randomNumbers = Math.floor(Math.random() * numbers.length);
-// console.log(numbers[randomNumbers]); // provides one random number
-
-// // Note that the array below does not include "]" or "_" or "\"
-// let symbols = [" ", "!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^", "`", "{", "|", "}", "~"]; 
-
-// let randomSymbols = Math.floor(Math.random() * numbers.length);
-// console.log(symbols[randomSymbols]); // provides one random symbol
